47ca8b597900ca579611393f5384a36604da7a68
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-10-20 11:39:50 -0500

    NavBar y logo terminado - 5 puntos

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import favicon from '../img/favicon.ico'
 
 export const PublicNavbar = () => (
   <nav>
     <section>
+      <img src={favicon} alt="Icon" />
       <Link to="/">Home</Link>
       <Link to="/questions">Questions</Link>
     </section>
   </nav>
+ 
 )
 
 export const PrivateNavbar = () => (
   <nav>
     <section>
+      <img src={favicon} alt="icon" />
       <Link to="/">Home</Link>
       <Link to="/questions">Questions</Link>
       <Link to="/new">New</Link>
